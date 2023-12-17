7510b746c8c58d1ccdbaa381bb581bb5f63543bf
martynovalexandr <2026martynov.ao@student.letovo.ru>
2023-12-17 19:26:23 +0300

    modified main.py (now we can also recognize speed limit signs)

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import cv2
 
 
 def red_brick(i, area_in):
+    #finding red area and looking for how much red color there are
     img = cv2.cvtColor(i, cv2.COLOR_BGR2HSV)
 
     lower1 = np.array([0, 150, 100])
@@ -22,11 +23,35 @@ def red_brick(i, area_in):
         for j in range(i.shape[1]):
             if gray[k, j] == 0:
                 red_area -= 1
-    print(red_area / area_in)
+
     if 0.5 < red_area / area_in < 0.8:
         return True
-    cv2.imshow("Image", gray)
-    cv2.waitKey(0)
+
+
+def speed_limit(i, area_in):
+    # finding balck area and looking for how much black color there are
+    img = cv2.cvtColor(i, cv2.COLOR_BGR2HSV)
+
+    lower1 = np.array([0, 0, 0])
+    upper1 = np.array([180, 255, 40])
+    lower2 = np.array([0, 0, 0])
+    upper2 = np.array([0, 250, 200])
+
+    mask1 = cv2.inRange(img, lower1, upper1)
+    mask2 = cv2.inRange(img, lower2, upper2)
+    full_mask = mask1 + mask2
+
+    img_masked = cv2.bitwise_and(img, img, mask=full_mask)
+    gray = cv2.cvtColor(img_masked, cv2.COLOR_BGR2GRAY)
+
+    black_area = i.shape[0] * i.shape[1]
+    for k in range(i.shape[0]):
+        for j in range(i.shape[1]):
+            if gray[k, j] == 0:
+                black_area -= 1
+
+    if 0.12 < black_area / area_in < 0.35:
+        return True
 
 
 picture_name = input()
@@ -48,4 +73,6 @@ for i in range(image.shape[0]):
             n -= 1
 
 if red_brick(image, n):
-    print('Red Brick')
+    print('Red Brick sign')
+elif speed_limit(image, n):
+    print('Speed limit sign')
