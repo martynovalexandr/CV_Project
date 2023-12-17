2bbccbf8d3d8eee82efcdc47cdb991258f999706
martynovalexandr <2026martynov.ao@student.letovo.ru>
2023-12-17 23:07:17 +0300

    new image and modified main.py (now we can also recognize 'No stopping' signs)

--- main.py
+++ main.py
@@ -1,84 +1,74 @@
-import math as m
 import numpy as np
 import cv2
 
 
-def red_brick(i, area_in):
-    #finding red area and looking for how much red color there are
+def colored_areas(i, area_in, color):
     img = cv2.cvtColor(i, cv2.COLOR_BGR2HSV)
 
-    lower1 = np.array([0, 150, 100])
-    upper1 = np.array([10, 255, 255])
-    lower2 = np.array([160, 150, 100])
-    upper2 = np.array([179, 255, 255])
+    if color == "red":
+        lower1 = np.array([0, 150, 100])
+        upper1 = np.array([10, 255, 255])
+        lower2 = np.array([160, 150, 100])
+        upper2 = np.array([179, 255, 255])
 
-    mask1 = cv2.inRange(img, lower1, upper1)
-    mask2 = cv2.inRange(img, lower2, upper2)
-    full_mask = mask1 + mask2
+        mask1 = cv2.inRange(img, lower1, upper1)
+        mask2 = cv2.inRange(img, lower2, upper2)
+
+        full_mask = mask1 + mask2
+
+    elif color == "white":
+        lower = np.array([0, 0, 210])
+        upper = np.array([255, 45, 255])
+
+        full_mask = cv2.inRange(img, lower, upper)
+
+    elif color == "black":
+        lower1 = np.array([0, 0, 0])
+        upper1 = np.array([180, 255, 40])
+        lower2 = np.array([0, 0, 0])
+        upper2 = np.array([0, 250, 200])
+
+        mask1 = cv2.inRange(img, lower1, upper1)
+        mask2 = cv2.inRange(img, lower2, upper2)
+        full_mask = mask1 + mask2
+
+    elif color == "blue":
+        lower = np.array([90, 60, 60])
+        upper = np.array([130, 255, 255])
+
+        full_mask = cv2.inRange(img, lower, upper)
+
     img_masked = cv2.bitwise_and(img, img, mask=full_mask)
-
     gray = cv2.cvtColor(img_masked, cv2.COLOR_BGR2GRAY)
-    red_area = i.shape[0] * i.shape[1]
+    col_area = i.shape[0] * i.shape[1]
     for k in range(i.shape[0]):
         for j in range(i.shape[1]):
             if gray[k, j] == 0:
-                red_area -= 1
+                col_area -= 1
+    return col_area / area_in
 
-    if 0.5 < red_area / area_in < 0.8:
+
+def red_brick(i, area_in):
+    #finding red area and looking for how much red color there are
+    if 0.5 < colored_areas(i, area_in, 'red') < 0.8:
         return True
 
 
 def speed_limit(i, area_in):
     # finding black area and looking for how much black color there are
-    img = cv2.cvtColor(i, cv2.COLOR_BGR2HSV)
-
-    lower1 = np.array([0, 0, 0])
-    upper1 = np.array([180, 255, 40])
-    lower2 = np.array([0, 0, 0])
-    upper2 = np.array([0, 250, 200])
-
-    mask1 = cv2.inRange(img, lower1, upper1)
-    mask2 = cv2.inRange(img, lower2, upper2)
-    full_mask = mask1 + mask2
-
-    img_masked = cv2.bitwise_and(img, img, mask=full_mask)
-    gray = cv2.cvtColor(img_masked, cv2.COLOR_BGR2GRAY)
-
-    black_area = i.shape[0] * i.shape[1]
-    for k in range(i.shape[0]):
-        for j in range(i.shape[1]):
-            if gray[k, j] == 0:
-                black_area -= 1
-
-    if 0.12 < black_area / area_in < 0.35:
+    if 0.12 < colored_areas(i, area_in, 'black') < 0.35:
         return True
 
 
 def no_road(i, area_in):
     # finding white area and looking for how much white color there are
-    img = cv2.cvtColor(i, cv2.COLOR_BGR2HSV)
-
-    sensitivity = 45
-    lower = np.array([0, 0, 255 - sensitivity])
-    upper = np.array([255, sensitivity, 255])
-
-    mask = cv2.inRange(img, lower, upper)
-
-    img_masked = cv2.bitwise_and(img, img, mask=mask)
-    gray = cv2.cvtColor(img_masked, cv2.COLOR_BGR2GRAY)
-
-    white_area = i.shape[0] * i.shape[1]
-
-    for k in range(i.shape[0]):
-        for j in range(i.shape[1]):
-            if gray[k, j] == 0:
-                white_area -= 1
-
-    if 0.85 < white_area / area_in:
+    if 0.85 < colored_areas(i, area_in, 'white'):
         return True
 
 
-
+def no_stopping(i, area_in):
+    if 0.35 < colored_areas(i, area_in, "blue") < 0.5 and 0.35 < colored_areas(i, area_in, "red") < 0.5:
+        return True
 
 picture_name = input()
 image = cv2.imread("test_images/"+picture_name+".jpeg")
@@ -104,3 +94,5 @@ elif speed_limit(image, n):
     print('Speed limit sign')
 elif no_road(image, n):
     print('End of all restricted areas sign')
+elif no_stopping(image, n):
+    print('No stopping sign')
